649fa794644c4706d8f38902aaef2861b8897dfc
mur4ik18 <o.o.kotov1999@gmail.com>
2024-07-30 14:51:20 +0200

    Add last changes before killing the project

--- spider/Leg.cpp
+++ spider/Leg.cpp
@@ -6,12 +6,50 @@
 #include <Servo.h>
 #include <Arduino.h>
 
-void Leg::Leg()
+#include <Adafruit_PWMServoDriver.h>
+
+void move(int address, int angle)
 {
-  
+  pwm.setPWM(address, 0, SERVOMIN+((SERVOMAX/270)*angle));
+}
+
+
+void Leg::Leg(
+            int coxa_address,
+            bool coxa_direction,
+            int femur_address,
+            bool femur_direction,
+            int tibia_address,
+            bool tibia_diretion
+)
+{
+  this->a_coxa = coxa_address;
+  this->a_femur = femur_address;
+  this->a_tibia = tibia_address;
+
+  this->d_coxa = coxa_direction;
+  this->d_femur = femur_direction;
+  this->d_tibia = tibia_direction;
+
+  this->coxaJoint = 0;
+  this->femurJoint= 0;
+  this->tibiaJoint= 0;
 }
 
 
 
+void Leg::move_coxa(int angle)
+{
+  move(a_coxa, angle);
+}
 
+void Leg::move_femur(int angle)
+{
+  move(a_femur, angle);
+}
+
+void Leg::move_tibia(int angle)
+{
+  move(a_tibia, angle);
+}
 
--- spider/Leg.h
+++ spider/Leg.h
@@ -3,7 +3,14 @@
 //
 #ifndef SPIDER_LEG_H
 #define SPIDER_LEG_H
-#include <Servo.h>
+
+#define SERVOMIN  150 // This is the 'minimum' pulse length count (out of 4096)
+#define SERVOMAX  600 // This is the 'maximum' pulse length count (out of 4096)
+#define USMIN  600 // This is the rounded 'minimum' microsecond length based on the minimum pulse of 150
+#define USMAX  2400 // This is the rounded 'maximum' microsecond length based on the maximum pulse of 600
+#define SERVO_FREQ 50 // Analog servos run at ~50 Hz updates
+
+Adafruit_PWMServoDriver pwm = Adafruit_PWMServoDriver();
 
 class Leg {
 public:
@@ -12,11 +19,24 @@ public:
   static int coxa = 10;
   static int femur = 20;
   static int tibia = 20;
-  // joints of each ..
+  // addresses
+  int a_coxa, a_femur, a_tibia;
+  // direction
+  bool d_coxa, d_femur, d_tibia;
+  // joints position of each ..
   int coxaJoint, femurJoint, tibiaJoint;
-  void Leg();
-
-
+  void Leg(
+            int coxa_address,
+            bool coxa_direction,
+            int femur_address,
+            bool femur_direction,
+            int tibia_address,
+            bool tibia_diretion
+            );
+  void move_coxa(int angle);
+  void move_femur(int angle);
+  void move_tibia(int angle);
+  
 };
 
 
